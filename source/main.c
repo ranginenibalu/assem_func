f4b15e9afa82ebdca11c2ead86d25f4b792d8f1f
rangineni balu <ranginenibalu@users.noreply.github.com>
2014-06-25 10:27:12 +0530

    Update main.c

--- source/main.c
+++ source/main.c
@@ -1,4 +1,15 @@
-
+/*******************************************************************************
+*
+*	Name:    main.c 
+*	Author:  Rangineni Balasubramanyam
+*	Date:    25-06-2014
+*
+*	Description: This file demonstrates the 
+*		     calling of functions written in assebly language	
+*		     from C
+*		     This project can be compiled in keil IDE	
+*
+*******************************************************************************/
 
 
 
